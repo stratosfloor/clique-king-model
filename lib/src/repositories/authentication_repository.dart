590b7869b5eab7b67e5d6022ee119557109e9b57
Emil Noren <stratosfloor@hotmail.com>
2023-10-30 15:45:43 +0100

    Create methods for registration and login with firebase authentication

--- lib/src/repositories/authentication_repository.dart
+++ lib/src/repositories/authentication_repository.dart
@@ -1,3 +1,5 @@
+import 'package:firedart/auth/exceptions.dart';
+import 'package:firedart/auth/user_gateway.dart';
 import 'package:firedart/firedart.dart';
 import 'package:meta/meta.dart';
 
@@ -7,6 +9,49 @@ final class AuthenticationRepository {
 
   AuthenticationRepository({required this.auth});
 
-    // TODO: Create methods for registration and login using Firebase Authentication.
+  // TODO: Create methods for registration and login using Firebase Authentication.
 
+  Future<User> createFirebaseAuth({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      return await auth.signUp(email, password);
+    } on AuthException catch (e) {
+      return Future.error(e);
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  Future<User> loginFirebaseAuth({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      return await auth.signIn(email, password);
+    } on AuthException catch (e) {
+      return Future.error(e.message);
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  Future<User> getUser() async {
+    try {
+      return await auth.getUser();
+    } catch (e) {
+      throw (e.toString());
+    }
+  }
+
+  void logoutFirebaseAuth() {
+    try {
+      if (auth.isSignedIn) {
+        auth.signOut();
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 }
