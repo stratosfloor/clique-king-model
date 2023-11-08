799210a1c7b41ef3a067fae376c7bc9241a0a2f7
Emil Noren <stratosfloor@hotmail.com>
2023-11-08 12:23:29 +0100

    Create model to for score

---
+++ lib/src/models/score.dart
@@ -0,0 +1,25 @@
+class Score {
+  final String userId;
+  final String username;
+  final int score;
+
+  Score({
+    required this.userId,
+    required this.username,
+    this.score = 0,
+  });
+
+  Map<String, dynamic> toMap() => {
+        'userId': userId,
+        'username': username,
+        'score': score,
+      };
+
+  factory Score.fromMap(Map<String, dynamic> map) {
+    return Score(
+      userId: map['userId'] as String,
+      username: map['username'] as String,
+      score: map['score'] as int,
+    );
+  }
+}
