2eb030c59d19158e6a5771ada55739785a48498f
Emil Noren <stratosfloor@hotmail.com>
2023-11-10 11:59:29 +0100

    Add uuid and remove scores attribute, it is added as collection in firestore upon creation

--- lib/src/models/clique.dart
+++ lib/src/models/clique.dart
@@ -2,34 +2,33 @@ import 'package:clique_king_model/src/models/score.dart';
 import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
 
+// TODO: Remove scores here, it as added as collection
 @immutable
 class Clique {
-  final String? id;
+  final String id;
   final String name;
-  final List<Score> scores;
 
   Clique.onCreate({
     required this.name,
-  })  : id = Uuid().v4(),
-        scores = const [];
+  }) : id = Uuid().v4();
 
   Clique({
     required this.id,
     required this.name,
-    required this.scores,
   });
 
   Map<String, dynamic> toMap() => {
         'id': id,
         'name': name,
-        'score': scores,
       };
 
   factory Clique.fromMap(Map<String, dynamic> map) {
     return Clique(
       name: map['name'] as String,
       id: map['id'] as String,
-      scores: (map['score'] as List).map((map) => Score.fromMap(map)).toList(),
     );
   }
+
+  @override
+  String toString() => toMap().toString();
 }
