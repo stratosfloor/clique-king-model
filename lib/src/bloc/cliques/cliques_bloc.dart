3d97e26fe52ccc7057e921641a659ea92d994c9a
Emil Noren <stratosfloor@hotmail.com>
2023-11-14 16:43:27 +0100

    Update cliques bloc

--- lib/src/bloc/cliques/cliques_bloc.dart
+++ lib/src/bloc/cliques/cliques_bloc.dart
@@ -1,14 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:clique_king_model/clique_king_model.dart';
 import 'package:clique_king_model/src/models/clique.dart';
+import 'package:clique_king_model/src/models/user.dart';
 import 'package:clique_king_model/src/repositories/clique_repository.dart';
 import 'package:meta/meta.dart';
+
+/*
+  EVENTS
+*/
 
 @immutable
 sealed class CliquesEvent {}
 
-// TODO: add CliquesLoadEvent or Reload
-// TODO: add newCliqueEvent
+final class CliquesLoadEvent extends CliquesEvent {}
+
+final class NewCliqueEvent extends CliquesEvent {
+  final String name;
+  final User user;
+
+  NewCliqueEvent({
+    required this.name,
+    required this.user,
+  });
+}
+
+/*
+  STATES
+*/
 
 @immutable
 sealed class CliquesState {}
@@ -25,6 +43,16 @@ final class CliquesLoadingSuccess extends CliquesState {
 
 final class CliquesLoadingFailure extends CliquesState {}
 
+final class NewCliqueSuccess extends CliquesState {
+  final List<Clique> cliques;
+
+  NewCliqueSuccess({
+    required this.cliques,
+  });
+}
+
+final class NewCliqueFailure extends CliquesState {}
+
 final class CliquesBloc extends Bloc<CliquesEvent, CliquesState> {
   final CliqueRepository _cliqueRepo; // passed in so it can be easily mocked
 
@@ -33,7 +61,27 @@ final class CliquesBloc extends Bloc<CliquesEvent, CliquesState> {
         super(CliquesInitial()) {
     on<CliquesEvent>(
       (event, emit) async {
-        switch (event) {}
+        switch (event) {
+          case CliquesLoadEvent():
+            emit(CliquesLoadingInProgress());
+            try {
+              final cliques = await _cliqueRepo.list();
+              emit(CliquesLoadingSuccess(cliques: cliques));
+            } on Exception catch (e) {
+              print(e);
+              emit(CliquesLoadingFailure());
+            }
+
+          case NewCliqueEvent():
+            if (await _cliqueRepo.exists(name: event.name)) {
+              emit(NewCliqueFailure());
+            } else {
+              final clique =
+                  await _cliqueRepo.create(name: event.name, user: event.user);
+              final cliques = await _cliqueRepo.list();
+              emit(NewCliqueSuccess(cliques: cliques));
+            }
+        }
       },
     );
   }
