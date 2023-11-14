0b3d80665fc26e4c592e1e79dff890f5f53d503a
Emil Noren <stratosfloor@hotmail.com>
2023-11-14 16:42:03 +0100

    Fix clique bloc

--- lib/src/bloc/clique/clique_bloc.dart
+++ lib/src/bloc/clique/clique_bloc.dart
@@ -1,10 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:clique_king_model/clique_king_model.dart';
 import 'package:clique_king_model/src/models/clique.dart';
+import 'package:clique_king_model/src/models/score.dart';
 import 'package:clique_king_model/src/models/user.dart';
 import 'package:clique_king_model/src/repositories/clique_repository.dart';
 import 'package:meta/meta.dart';
 
+/*
+  EVENTS 
+*/
 @immutable
 sealed class CliqueEvent {}
 
@@ -14,7 +18,21 @@ final class CliqueLoad extends CliqueEvent {
   CliqueLoad({required this.id});
 }
 
-final class CliqueIncreaseScore extends CliqueEvent {}
+final class CliqueIncreaseScore extends CliqueEvent {
+  final String cliqueId;
+  final String userId;
+  final int score;
+
+  CliqueIncreaseScore({
+    required this.cliqueId,
+    required this.userId,
+    required this.score,
+  });
+}
+
+/*
+  STATES
+*/
 
 @immutable
 sealed class CliqueState {}
@@ -25,9 +43,9 @@ final class CliqueLoadingInProgress extends CliqueState {}
 
 final class CliqueLoadingSuccess extends CliqueState {
   final Clique clique;
-  final List<User> participants;
+  final List<Score> scores;
 
-  CliqueLoadingSuccess({required this.clique, required this.participants});
+  CliqueLoadingSuccess({required this.clique, required this.scores});
 }
 
 final class CliqueLoadingFailure extends CliqueState {}
@@ -36,21 +54,38 @@ final class CliqueBloc extends Bloc<CliqueEvent, CliqueState> {
   final UserRepository _userRepo; // passed in so it can be easily mocked
   final CliqueRepository _cliqueRepo; // passed in so it can be easily mocked
 
-  CliqueBloc(
-      {required UserRepository userRepository,
-      required CliqueRepository cliqueRepository})
-      : _userRepo = userRepository,
+  CliqueBloc({
+    required UserRepository userRepository,
+    required CliqueRepository cliqueRepository,
+  })  : _userRepo = userRepository,
         _cliqueRepo = cliqueRepository,
         super(CliqueInitial()) {
     on<CliqueEvent>(
       (event, emit) async {
         switch (event) {
           case CliqueLoad():
-          // TODO: Handle this case.
+            emit(CliqueLoadingInProgress());
+            try {
+              final clique = await cliqueRepository.read(id: event.id);
+              final documentid =
+                  await cliqueRepository.getDocumentid(name: clique.name);
+              final scores =
+                  await cliqueRepository.listScores(docid: documentid);
+              emit(CliqueLoadingSuccess(clique: clique, scores: scores));
+            } on Exception catch (e) {
+              print(e);
+              emit(CliqueLoadingFailure());
+            }
+
           case CliqueIncreaseScore():
-          // TODO: Handle this case.
-          // TODO: This is definitely up to you.
-          //       What kind of action in the app should Increase Score?
+            final oldScore = await _cliqueRepo.getScore(
+                docid: event.cliqueId, userId: event.userId);
+            final newScore = oldScore.score + event.score;
+
+            _cliqueRepo.updateScores(
+                cliqueId: event.cliqueId,
+                userId: event.userId,
+                score: newScore);
         }
       },
     );
